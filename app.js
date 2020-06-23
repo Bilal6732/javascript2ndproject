8313cce05b87610b83284c782051556f1abe60e9
Bilal6732 <bilalsiddique6732@gmail.com>
2020-06-23 23:30:37 +0500

    Now ready javascript 2nd project

---
+++ app.js
@@ -0,0 +1,623 @@
+
+                                                    // Task 1
+
+// var a =prompt("Enter your first name")
+
+
+
+// var b =prompt("Enter your last name")
+
+
+
+// var c=(a+b)
+
+
+
+// document.write("<h1>Asslam u alaikum\n\n</h1>"+c.fontsize(30))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                   // Task 2
+
+
+
+// var str=prompt("Enter your favourite mbl name")
+
+// var a=str
+
+// var n= str.length
+
+// document.write("My favourite phone is \n: "+a+"<br>")
+
+// document.write("Length of string is : "+n)
+
+
+
+
+
+
+
+                                                // Task 3
+
+
+
+// var str =("Pakistan")
+
+// var a=str.indexOf("k")
+
+// document.write("String : "+str+"<br>")
+
+// document.write("Index of 'n' : "+a)
+
+
+
+
+
+                                                // Task 4
+
+
+// var str="Hello World"
+
+// var a=str.lastIndexOf("l")
+
+// document.write("String : "+str+"<br>")
+
+// document.write("Last index of 'l' : "+a)
+
+
+
+
+
+
+
+                                               // Task 5
+
+                                        
+
+// var str="Pakistani"
+
+// var a=str.charAt(3)
+
+// document.write("String : "+str+"<br>")
+
+// document.write("Character at index 3 : "+a)
+
+
+
+
+
+
+
+
+
+
+                                          // Task 6
+
+
+
+
+
+// var a =prompt("Enter your first name")
+
+// var b =prompt("Enter your last name")
+
+// var c=(a+b)
+
+// document.write("<h1>Asslam u alaikum\n\n</h1>"+c.fontsize(30))
+
+
+
+
+
+ 
+                                           // Task 7
+
+
+
+// var str="Hyderabad"
+
+// a=str.slice(5)
+
+// document.write(str+"<br>")
+
+// document.write("After Replacement : Islam"+a)
+
+
+
+
+
+
+                                           // Task 8
+
+
+// var text="Ali and Sami are best friends. They play cricket and football together."; 
+
+// var str="and"
+
+// for(i=0;i<text.length;i++){
+
+//     if(text.slice(i,i+3)==="and"){
+
+//         text=text.slice(0,i)+"&"+text.slice(i+3)
+       
+        
+//     }
+
+// }
+
+// document.write(text)
+
+
+
+
+
+
+
+                                            // Task 9
+
+
+// var num=472
+
+// document.write("Value type is : "+typeof num+  "  and value is : "+num+"<br>")
+
+// var n=num.toString()
+
+// document.write("Type is when convert into string is  : "+typeof n+"<br>")
+
+// var num1="BILAL"
+
+// document.write("Value type is : "+typeof num1+"  and value is : "+num1+"<br>")
+
+// var nn=parseInt(num1)
+
+// document.write("Type is when is convert into number is  : "+typeof nn)
+
+
+
+
+
+
+                                               // Task 10
+
+
+
+
+
+                                        
+// var str=prompt("Enter any string value in small letter and i make it all letter in upper case")
+
+// document.write("User input is : "+str+" <br>")
+
+// var stra=str.toUpperCase()
+
+// document.write("UpperCase is : "+stra)
+
+
+
+
+
+
+
+
+                                           // Task 11
+
+
+// var a="Hamza"
+
+// var n=a.slice(0,1)
+
+// var x=n.toUpperCase()
+
+// var z=a.slice(1)
+
+// document.write(x+z)
+
+
+
+
+
+
+
+//                                            // Task 12
+
+
+
+
+//  var d = 109.65;
+
+//  document.write("Number : "+d+"<br>")
+
+//  a=(d + '')
+
+//  var a =a.replace('.','');
+
+
+//  document.write("Result : "+a);
+
+
+
+
+                           
+
+
+
+                                                // Task 13
+
+//  var matchfound='no'
+
+//  var a=prompt("Enter User name")
+
+//  for(i=0;i<=a.length;i++){
+
+//     if(a[i]==="!" || a[i]==="@" || a[i]==="." || a[i]==="," ){
+
+//         matchfound='Yes'
+
+//         alert("Please enter valid user name")
+        
+        
+//     }
+    
+//   }
+
+//   if(matchfound==='no'){
+
+//     alert("Welcome")
+    
+
+// }
+ 
+
+ 
+
+                                           // Task 14
+
+
+
+                                           
+
+
+
+// var g;
+// var matchfound='no'
+
+// var a=["cake","apple pie","cookie","chips","patties"]
+
+// var b=prompt("Enter any product")
+
+// b=b.toLowerCase()
+// for(i=0;i<=b.length;i++){
+
+//     if(b === a[i] ){
+
+//         matchfound='yes'
+//         var g=a.indexOf(a[i])
+//      alert("It's available on bakery at index : "+g);
+      
+
+
+         
+//     }
+    
+// } if(matchfound==="no"){
+
+//     alert("Its not available on bakery")
+
+    
+
+// }
+
+
+
+
+
+
+
+
+
+                                                        //   Task # 15
+
+
+
+
+
+
+
+
+ // var a=prompt("Enter Passoword")
+
+// var b=a.slice(0,1)
+
+// var c=a.slice(0)
+
+// for(i=0;i<=a.length;i++){
+   
+//     if(a[i]==="!" || a[i]==="@" || a[i]==="#" || a[i]==="$" || a[i]==="%" || a[i]==="^" || a[i]==="&" || a[i]==="*" || a[i]==="(" || a[i]===")" || a[i]==="_" || a[i]==="+" || a[i]==="-" || a[i]==="=" || a[i]==="/" || a[i]==="|" || a[i]==="]" || a[i]==="[" || a[i]==="{" || a[i]==="}" ||a[i]==="?"  || a[i]===">" || a[i]==="<" || a[i]==="," || a[i]==="." || a[i]===";" || a[i]==="'" || a[i]===":" || a[i]==="~" || a[i]==="`"  ){
+
+//     alert("Plz enter alphabates and number only")
+
+//     } 
+
+// }if(b==="0" || b==="1" || b==="2" || b==="3" || b==="4" || b==="5" || b==="6" || b==="7" || b==="8" || b==="9"){
+
+//     alert("plz not enter first letter in numeric")
+
+// }
+
+// if(c.length<6){alert("It must at least 6 characters long ")}
+
+
+
+
+
+
+                                                                  
+
+
+
+                                                                     //   Task # 16
+
+
+
+
+
+//  var str = "University Of Karachi";
+
+//  var res = str.split("");
+
+//  for(i=0;i<res.length;i++){
+
+//     document.write(res[i]+"<br>")
+
+// }
+                                                                     
+
+
+
+                           
+
+
+
+
+                                                              //   Task # 17
+
+
+
+
+
+
+// var a=prompt("Enter value")
+
+// var b=a.substr(-1)
+
+
+// alert(b)
+
+
+
+
+
+                                                              //   Task # 18
+
+
+
+
+
+                               
+
+
+
+
+
+                                                                // chapter#26-30
+
+
+
+                    
+
+                                                            
+
+
+
+                                                                // task # 1
+
+
+
+// var a=("3.45214")
+
+// document.write("number : "+a+"<br>")
+
+// var b=Math.round(3.45214);
+
+// document.write("Round off : "+b+"<br>")
+
+// var c=Math.floor(3.45214);
+
+// document.write("Floor value : "+c+"<br>")
+
+// var d=Math.ceil(3.45214)
+
+
+// document.write("ceil value : "+d+"<br>")
+
+
+
+
+
+
+
+                                                              // task # 2
+                                                              
+
+
+
+
+
+// var a=("-3.45214")
+
+// document.write("number : "+a+"<br>")
+
+// var b=Math.round(-3.45214);
+
+// document.write("Round off : "+b+"<br>")
+
+// var c=Math.floor(-3.45214);
+
+// document.write("Floor value : "+c+"<br>")
+
+// var d=Math.ceil(-3.45214)
+
+
+// document.write("ceil value : "+d+"<br>")
+
+
+
+
+
+
+
+
+
+
+                                                           // task # 3
+
+
+
+
+
+
+
+
+var b = -7.25
+
+
+document.write("before "+b+"<br>")
+
+
+var b = Math.abs(-7.25);
+
+
+document.write("After absolute "+b)
+
+
+
+
+
+
+
+
+                                                            // task # 4
+
+
+
+
+var diceRoll = Math.floor( Math.random() * 6 ) +1;
+
+alert('You rolled a ' + diceRoll);
+
+
+
+
+
+
+
+
+
+                                                             // task # 5
+
+
+
+
+var heads = 0;
+var tails = 0;
+function click() {  
+    x = (Math.floor(Math.random() * 2) == 0);
+    if(x){
+        flip("heads");
+    }else{
+        flip("tails");
+    }
+};
+function flip(coin) {
+    document.getElementById("result").innerHTML = coin;
+};
+
+
+
+
+
+
+
+
+                                                            // task # 7
+
+
+
+
+
+
+
+function onlyNumber(fld) {
+     if(fld.value.match(/[^0-9.]/)) {
+         fld.value=fld.value.replace(/[^0-9.]/g,'');
+     }
+}
+
+function convertUnit(lbs, kilo) {
+    retValue = 0;
+
+    if (isNaN (kilo)) { alert ('Non-numeric value');  return 0; }
+        kilo = parseFloat (kilo);
+        var factor = 0.4536;
+
+    if (lbs == 'kg2lb') {
+         retValue = kilo/factor;
+    }
+
+    else if (lbs == 'lb2kg') {
+         retValue = kilo*factor;
+    }
+
+    return retValue;
+}
+
+
+
+
+                                                                
+                                                            // task # 8
+
+
+
+
+
+
+
+
+var num = Math.ceil(Math.random() * 10);
+console.log(num);
+var gnum = prompt('Guess the number between 1 and 10 inclusive');
+if (gnum == num) console.log('Matched');else console.log('Not matched, the number was ' + gnum);
+
+
+
+
+
+
+
+
+
+                    
+
+
